253cfb20de09c5862e1b4adf83caeb6ad841d824
SOCSCI1SS3 <socsci1ss3@gmail.com>
2024-12-27 13:31:02 -0500

    Add files via upload

---
+++ vue.js
@@ -0,0 +1,33 @@
+const app = Vue.createApp({
+      data() {
+        return {
+          showTextareas: true,
+          showReflections: false,
+          fullname: '',
+          c1r1: '', 
+          c1r2: '', 
+          c2r1: '', 
+          c2r2: '', 
+          c3r1: '', 
+          c3r2: '', 
+          c4r1: '',
+          c4r2: ''
+        }
+    }, 
+  methods: {
+    savePdf() {
+      this.showTextareas = false;
+      this.showReflections = true;
+     	let formElement = document.getElementById('pdfForm');
+	   	html2pdf().from(formElement).save('selfAssessmentRubric.pdf')
+	    }, 
+    
+    editTextareas() {
+      this.showTextareas = true;
+      this.showReflections = false;
+    }
+  }
+  
+  })
+  
+ app.mount('#app')
